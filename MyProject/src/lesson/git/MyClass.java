2ca573734409def5d03c8b0fe17d14a093cf8e27
Atsuko Yamazaki <atsuko.yamazaki@yh.nackademin.se>
2012-11-08 10:40:10 +0100

    Change

--- MyProject/src/lesson/git/MyClass.java
+++ MyProject/src/lesson/git/MyClass.java
@@ -7,8 +7,8 @@ public class MyClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitHub!!");
-		System.out.println("test2");
+		System.out.println("GitHub2!!");
+		System.out.println("test12");
 
 	}
 
